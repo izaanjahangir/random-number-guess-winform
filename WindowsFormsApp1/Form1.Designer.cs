bc82e27523c92e2d8fb74ed093f4d72597197563
izaan jahangir <izaanjahangir2@gmail.com>
2022-02-10 01:45:40 +0500

    added random guess game

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_submit = new System.Windows.Forms.Button();
+            this.lb_name2 = new System.Windows.Forms.Label();
+            this.tb_guess = new System.Windows.Forms.TextBox();
+            this.lb_name = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb_answer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_submit
+            // 
+            this.btn_submit.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_submit.Location = new System.Drawing.Point(202, 199);
+            this.btn_submit.Name = "btn_submit";
+            this.btn_submit.Size = new System.Drawing.Size(110, 33);
+            this.btn_submit.TabIndex = 0;
+            this.btn_submit.Text = "Submit";
+            this.btn_submit.UseVisualStyleBackColor = true;
+            this.btn_submit.Click += new System.EventHandler(this.btn_submit_Click);
+            // 
+            // lb_name2
+            // 
+            this.lb_name2.AutoSize = true;
+            this.lb_name2.Location = new System.Drawing.Point(72, 153);
+            this.lb_name2.Name = "lb_name2";
+            this.lb_name2.Size = new System.Drawing.Size(0, 13);
+            this.lb_name2.TabIndex = 1;
+            // 
+            // tb_guess
+            // 
+            this.tb_guess.Location = new System.Drawing.Point(60, 93);
+            this.tb_guess.Name = "tb_guess";
+            this.tb_guess.Size = new System.Drawing.Size(403, 20);
+            this.tb_guess.TabIndex = 2;
+            // 
+            // lb_name
+            // 
+            this.lb_name.AutoSize = true;
+            this.lb_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_name.Location = new System.Drawing.Point(55, 28);
+            this.lb_name.Name = "lb_name";
+            this.lb_name.Size = new System.Drawing.Size(408, 29);
+            this.lb_name.TabIndex = 3;
+            this.lb_name.Text = "Guess a random number from 0 to 10";
+            this.lb_name.Click += new System.EventHandler(this.lb_name_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter your guess here";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lb_answer
+            // 
+            this.lb_answer.AutoSize = true;
+            this.lb_answer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_answer.ForeColor = System.Drawing.Color.Red;
+            this.lb_answer.Location = new System.Drawing.Point(177, 163);
+            this.lb_answer.Name = "lb_answer";
+            this.lb_answer.Size = new System.Drawing.Size(165, 20);
+            this.lb_answer.TabIndex = 5;
+            this.lb_answer.Text = "Enter your guess here";
+            this.lb_answer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lb_answer.Visible = false;
+            this.lb_answer.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(521, 254);
+            this.Controls.Add(this.lb_answer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lb_name);
+            this.Controls.Add(this.tb_guess);
+            this.Controls.Add(this.lb_name2);
+            this.Controls.Add(this.btn_submit);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Random guess game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_submit;
+        private System.Windows.Forms.Label lb_name2;
+        private System.Windows.Forms.TextBox tb_guess;
+        private System.Windows.Forms.Label lb_name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb_answer;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int randomNumber;
+        bool isGameEnded = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            initGame();
+            
+   
+        }
+
+        void initGame() {
+            randomNumber = getRandomNumber();
+            lb_answer.Visible = false;
+            tb_guess.ReadOnly = false;
+            isGameEnded = false;
+            btn_submit.Text = "Submit";
+            tb_guess.Clear();
+
+            Console.WriteLine("Random number is: " + randomNumber);
+        }
+
+        void endGame() {
+            tb_guess.ReadOnly = true;
+            isGameEnded = true;
+            btn_submit.Text = "Play Again";
+        }
+
+        int getRandomNumber() {
+            var rand = new Random();
+            return rand.Next(0, 10);
+        }
+
+        private void showError(string errorMessage) {
+            lb_answer.Text = errorMessage;
+            lb_answer.ForeColor = Color.Red;
+            lb_answer.Visible = true;
+        }
+
+        private void showSuccess(string successMessage)
+        {
+            lb_answer.Text = successMessage;
+            lb_answer.ForeColor = Color.Green;
+            lb_answer.Visible = true;
+        }
+
+        private void btn_submit_Click(object sender, EventArgs e)
+        {
+            if(isGameEnded)
+            {
+                initGame();
+                return;
+            }
+            string guess = tb_guess.Text;
+            int guessedNumber;
+            bool isNumber = int.TryParse(guess, out guessedNumber);
+
+            if (string.IsNullOrEmpty(guess))
+            {
+                showError("Come on, first guess something");
+                return;
+            }
+
+            if(!isNumber)
+            {
+                showError("Please write a valid number");
+                return;
+            } 
+
+            Console.WriteLine("randomNumber first: " + randomNumber);
+            Console.WriteLine("guess: " + guess);
+            Console.WriteLine("guessedNumber: " + guessedNumber);
+            Console.WriteLine("randomNumber: " + randomNumber);
+
+            if (guessedNumber == randomNumber)
+            {
+                showSuccess("Damn, your guess is soo good");
+                endGame();
+            }
+            else if(guessedNumber < randomNumber)
+            {
+                showError("Nope, increase your guess");
+            }
+            else if (guessedNumber > randomNumber)
+            {
+                showError("Nope, decrease your guess");
+            }
+            else
+            {
+                showError("Nope, try again");
+            }
+        }
+
+        private void lb_name_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
